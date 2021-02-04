412d2e624cf305785d9dcb3bdda4a09627ebca30
gaëlor <thomas.couilleaux@metsys.fr>
2021-02-04 11:13:17 +0100

    master

--- Scripts/CreateWorkbooks.ps1
+++ Scripts/CreateWorkbooks.ps1
@@ -8,8 +8,8 @@ param(
 #Adding AzSentinel module
 Install-Module AzSentinel -AllowClobber -Scope CurrentUser -Force
 Import-Module AzSentinel
-Install-Module Az.OperationalInsights -Scope CurrentUser -Force
-Import-Module Az.OperationalInsights
+Install-Module Az.Resources -Scope CurrentUser -Force
+Import-Module Az.Resources
 
 #Getting all workspaces from file
 $workspaces = Get-Content -Raw -Path $OnboardingFile | ConvertFrom-Json
