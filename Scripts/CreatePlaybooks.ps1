6820ba6bc9d1c17ce3a7960b0213a72cbe7e320c
gaëlor <thomas.couilleaux@metsys.fr>
2021-02-05 15:49:12 +0100

    master

--- Scripts/CreatePlaybooks.ps1
+++ Scripts/CreatePlaybooks.ps1
@@ -28,7 +28,7 @@ foreach ($item in $workspaces.deployments){
     Write-Host "Processing resourcegroup $($item.resourcegroup) ..."
 
     #Getting all playbooks from folder
-    $armTemplateFiles = Get-ChildItem -Recurse -Path $PlaybooksFolder -Filter *CAIPAdress.json
+    $armTemplateFiles = Get-ChildItem -Recurse -Path $PlaybooksFolder -Filter *NSGIPAdress.json
 
     Write-Host "Files are: " $armTemplateFiles
 
