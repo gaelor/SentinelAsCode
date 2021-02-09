82825eafc0d890e5daa0cdaa8196f80c3b3eef08
gaëlor <thomas.couilleaux@metsys.fr>
2021-02-09 14:10:11 +0100

    master

--- tmp/mdgenerator.py
+++ tmp/mdgenerator.py
@@ -3,12 +3,13 @@ import json
 with open('c:\\Users\\thomas.couilleaux\\OneDrive - METSYS\\SentinelAsCode\\SentinelAsCode\\HuntingRules\\hunting-rules.json', 'r') as huntingrules:
     myjson = json.load(huntingrules)
 
+print('![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")')
+print('# Hunting Rules')
+
 for huntingrule in myjson:
     for item in myjson[huntingrule]:
-        for key,value in item.items():
-            if key == 'displayName' or key == 'author':
-                print('![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")')
-                print('# Hunting Rules')
-                print('> ' + value)
-                print('## Hunt Tagsvalue')
-                print('**Author:** [' + value + '](https://www.metsys.fr/)')
+#        for value in item.items():
+#            if key == 'displayName' or key == 'author':
+        print('> ' + item["displayName"])
+        print('## Hunt Tagsvalue')
+        print('**Author:** [' + item["author"] + '](https://www.metsys.fr/)')
--- tmp/test3.md
+++ tmp/test3.md
@@ -1,20 +1,8 @@
 ![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")
 # Hunting Rules
-> thomas couilleaux
-## Hunt Tagsvalue
-**Author:** [thomas couilleaux](https://www.metsys.fr/)
-![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")
-# Hunting Rules
 > HuntingRule01
 ## Hunt Tagsvalue
-**Author:** [HuntingRule01](https://www.metsys.fr/)
-![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")
-# Hunting Rules
-> thomas couilleaux
-## Hunt Tagsvalue
 **Author:** [thomas couilleaux](https://www.metsys.fr/)
-![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")
-# Hunting Rules
 > HuntingRule02
 ## Hunt Tagsvalue
-**Author:** [HuntingRule02](https://www.metsys.fr/)
+**Author:** [thomas couilleaux](https://www.metsys.fr/)
