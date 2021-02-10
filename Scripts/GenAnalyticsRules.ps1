9c62039290bbec868e92bb7bf5af92d84f7da354
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-10 14:42:34 +0100

    master

--- Scripts/GenAnalyticsRules.ps1
+++ Scripts/GenAnalyticsRules.ps1
@@ -1,7 +1,8 @@
 param(
-    [Parameter(Mandatory=$true)]$OnboardingFolder,
+    [Parameter(Mandatory=$true)]$AnalyticsRulesFolder,
     [Parameter(Mandatory=$true)]$TenantID,
     [Parameter(Mandatory=$true)]$SubscriptionID,
+    [Parameter(Mandatory=$true)]$Workspace,
     [Parameter(Mandatory=$true)]$GIT_USER,
     [Parameter(Mandatory=$true)]$GIT_EMAIL,
     [Parameter(Mandatory=$true)]$Azure_User,
@@ -17,33 +18,8 @@ $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -
 
 Connect-AzAccount -Credential $Credential -Tenant $TenantID -Subscription $SubscriptionID
 
-#Getting all workspaces
-$workspaces = Get-AzOperationalInsightsWorkspace | Where-Object -Property Name -notmatch "Default" | Select-Object Name, ResourceGroupName
-
 $Date = Get-Date -Format "ddMMyyyy"
 
-$Onboarding =  "{   `"tenant`": `"$TenantID`"," + "`r`n"
-$Onboarding += "    `"subscription`": `"$SubscriptionID`"," + "`r`n"
-$Onboarding += "    `"deployments`": [" + "`r`n"
-foreach ($item in $workspaces){
-    if ($item -ne $workspaces[-1]){
-        $Onboarding += "        {" + "`r`n"
-        $Onboarding += "            `"resourcegroup`": `"" + $item.ResourceGroupName +"`"," + "`r`n"
-        $Onboarding += "            `"workspace`": `"" + $item.Name + "`"," + "`r`n"
-        $Onboarding += "        }," + "`r`n"
-    }
-    else {
-        $Onboarding += "        {" + "`r`n"
-        $Onboarding += "            `"resourcegroup`": `"" + $item.ResourceGroupName + "`"," + "`r`n"
-        $Onboarding += "            `"workspace`": `"" + $item.Name + "`"" + "`r`n"
-        $Onboarding += "        }" + "`r`n"
-    }
-}
-$Onboarding += "    ]" + "`r`n"
-$Onboarding += "}" + "`r`n"
-Out-File -Path $OnboardingFolder\onboarding_$Date.json -InputObject $Onboarding
-git config --global user.name $GIT_USER
-git config --global user.email $GIT_EMAIL
-git add $OnboardingFolder\onboarding_$Date.json
-git commit -am "Automated OnboardFile"
-git push
+Export-AzSentinel -WorkspaceName $Workspace -OutputFolder $AnalyticsRulesFolder$Workspace"_" -Kind Templates
+Remove-Item -r $AnalyticsRulesFolder$Workspace"_\"
+Move-Item -Path $AnalyticsRulesFolder$Workspace"_Templates_*"$Date".json" $AnalyticsRulesFolder$Workspace"_"$Date"_analytics-rules.json"
