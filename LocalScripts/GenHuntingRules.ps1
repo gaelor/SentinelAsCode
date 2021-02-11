0c23a9380aa0ac7710cae507a51c5a01bd0d744d
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-11 09:41:09 +0100

    master

--- LocalScripts/GenHuntingRules.ps1
+++ LocalScripts/GenHuntingRules.ps1
@@ -4,7 +4,7 @@ $Files = Get-ChildItem -Path '.\tmp\Azure-Sentinel\'  -Filter *.yaml -Recurse -F
 $HuntingRulesTemplate = "{`r`n"
 $HuntingRulesTemplate += "  `"hunting`": [`r`n"
 foreach ($file in $Files){
-    if ($file -ne $Files[-1] -And $file -ne $Files[0]){
+    if ($file -ne $Files[-1]) {
         $content = ''
         $filecontent = Get-Content -Path tmp\Azure-Sentinel\$file
         foreach ($line in $filecontent) { $content = $content + "`n" + $line }
@@ -18,7 +18,7 @@ foreach ($file in $Files){
         $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + $huntingrules.tactics.tostring().replace(" ","`",`"") + "`"`r`n      ]`r`n"
         $HuntingRulesTemplate += "    },`r`n"
     }
-    elseif ($file -ne $Files[0]){
+    else {
         $content = ''
         $filecontent = Get-Content -Path tmp\Azure-Sentinel\$file
         foreach ($line in $filecontent) { $content = $content + "`n" + $line }
@@ -32,7 +32,6 @@ foreach ($file in $Files){
         $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + $huntingrules.tactics.tostring().replace(" ","`",`"") + "`"`r`n      ]`r`n"
         $HuntingRulesTemplate += "    }`r`n"
     }
-    else {}
 }
 $HuntingRulesTemplate += "  ]`r`n"
 $HuntingRulesTemplate += "}`r`n"
