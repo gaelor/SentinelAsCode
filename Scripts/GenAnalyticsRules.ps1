03143d31844b74d01f4215a570feef0eaaf4bd92
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-11 17:11:28 +0100

    ;qster

--- Scripts/GenAnalyticsRules.ps1
+++ Scripts/GenAnalyticsRules.ps1
@@ -18,6 +18,6 @@ Connect-AzAccount -Credential $Credential -Tenant $TenantID -Subscription $Subsc
 
 $Date = Get-Date -Format "ddMMyyyy"
 
-Export-AzSentinel -WorkspaceName $Workspace -OutputFolder $AnalyticsRulesFolder"_" -Kind Templates
+Export-AzSentinel -WorkspaceName $Workspace -OutputFolder $AnalyticsRulesFolder"_" -Kind Templates -Encoding ASCII
 Remove-Item -r $AnalyticsRulesFolder"_\"
 Move-Item -Path $AnalyticsRulesFolder"_Templates_*"$Date".json" $AnalyticsRulesFolder"analytics-rules_"$Date".json"
