639e809726d8bf224c64f5ef2446c8b0e3fe58e6
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-11 12:24:56 +0100

    master

--- LocalScripts/GenWortellHuntingRules.ps1
+++ LocalScripts/GenWortellHuntingRules.ps1
@@ -3,38 +3,30 @@ param(
 )
 git clone https://github.com/wortell/KQL.git tmp/Wortell_KQL
 $Date = Get-Date -Format "ddMMyyyy"
-$Files = Get-ChildItem -Path '.\tmp\Azure-Sentinel\'  -Filter *.yaml -Recurse -File -Name | Select-String -Pattern "Hunting Queries"
+$Files = Get-ChildItem -Path '.\tmp\Wortell_KQL\'  -Filter *.txt -Recurse -File -Name
 $HuntingRulesTemplate = "{`r`n"
 $HuntingRulesTemplate += "  `"hunting`": [`r`n"
 foreach ($file in $Files){
     if ($file -ne $Files[-1]) {
-        $content = ''
-        $filecontent = Get-Content -Path tmp\Azure-Sentinel\$file
-        foreach ($line in $filecontent) { $content = $content + "`n" + $line }
-        $huntingrules = ConvertFrom-YAML $content
-        $tactics = $huntingrules.tactics -join "`",`""
+        $filecontent = Get-Content -Path tmp\Wortell_KQL\$file
         $HuntingRulesTemplate += "    {`r`n"
-        $HuntingRulesTemplate += "      `"author`": `"microsoft`",`r`n"
-        $HuntingRulesTemplate += "      `"displayName`": `"" + $huntingrules.name + "`",`r`n"
-        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/" + $file.tostring().replace("\", "/").replace(" ","%20") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"description`": `"" + $huntingrules.description.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"query`": `"" + $huntingrules.query.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + $tactics + "`"`r`n      ]`r`n"
+        $HuntingRulesTemplate += "      `"author`": `"wortell`",`r`n"
+        $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/wortell/KQL/master/" + $file.tostring() + "`",`r`n"
+        $HuntingRulesTemplate += "      `"description`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"Collection`"`r`n      ]`r`n"
         $HuntingRulesTemplate += "    },`r`n"
     }
     else {
-        $content = ''
-        $filecontent = Get-Content -Path tmp\Azure-Sentinel\$file
-        foreach ($line in $filecontent) { $content = $content + "`n" + $line }
-        $huntingrules = ConvertFrom-YAML $content
-        $tactics = $huntingrules.tactics -join "`",`""
+        $filecontent = Get-Content -Path tmp\Wortell_KQL\$file
         $HuntingRulesTemplate += "    {`r`n"
-        $HuntingRulesTemplate += "      `"author`": `"microsoft`",`r`n"
-        $HuntingRulesTemplate += "      `"displayName`": `"" + $huntingrules.name + "`",`r`n"
-        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/" + $file.tostring().replace("\", "/").replace(" ","%20") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"description`": `"" + $huntingrules.description.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"query`": `"" + $huntingrules.query.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + $tactics + "`"`r`n      ]`r`n"
+        $HuntingRulesTemplate += "      `"author`": `"wortell`",`r`n"
+        $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/wortell/KQL/master/" + $file.tostring() + "`",`r`n"
+        $HuntingRulesTemplate += "      `"description`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"Collection`"`r`n      ]`r`n"
         $HuntingRulesTemplate += "    }`r`n"
     }
 }
@@ -42,4 +34,4 @@ $HuntingRulesTemplate += "  ]`r`n"
 $HuntingRulesTemplate += "}`r`n"
 Out-File -Path $HuntingRulesFolder\"Wortell_"$Date"_hunting-rules.json" -InputObject $HuntingRulesTemplate
 Start-Sleep -s 20
-Remove-Item -Path tmp/kql –Recurse -Force
+Remove-Item -Path tmp/Wortell_KQL –Recurse -Force
