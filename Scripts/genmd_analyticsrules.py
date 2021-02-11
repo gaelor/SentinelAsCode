46146259f973f153610029c1c1d2e7e551d9cd74
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-11 16:16:15 +0100

    master

--- Scripts/genmd_analyticsrules.py
+++ Scripts/genmd_analyticsrules.py
@@ -1,21 +1,20 @@
 import json
 import sys
 
-with open(sys.argv[1], 'r') as huntingrules:
-    myjson = json.load(huntingrules)
+with open(sys.argv[1], 'r') as analyticsrules:
+    myjson = json.load(analyticsrules)
 
 print('![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")')
-print('# Hunting Rules')
+print('# Analytics Rules')
 
-for huntingrule in myjson:
-    for item in myjson[huntingrule]:
+for analyticsrule in myjson:
+    for item in myjson[analyticsrule]:
         print('## ' + item["displayName"])
-        print('### Hunt Tags\n')
-        print('> Author: [' + item["author"] + '](https://www.metsys.fr/)' + '\n')
-        print('> Reference: [Link to medium post](' + item["reference"] + ')' + '\n')
-        print('### ATT&CK Tags\n')
-        print('> Tactics: ' + str(item["tactics"]) + '\n')
-        print('### Hunt details\n')
+        print('### Informations\n')
+        print('> productFilter: ' + item["productFilter"] + '\n')
+        print('> lastUpdatedDateUTC: ' + item["lastUpdatedDateUTC"] + '\n')
+        print('> createdDateUTC: ' + item["createdDateUTC"] + '\n')
+        print('### Details\n')
+        print('> Requirements:\n')
+        print('```' + item["requiredDataConnectors"] + '```')
         print('> Description: ' + item["description"] + '\n')
-        print('> Query:\n')
-        print('```C#' + item["query"] + '```')
