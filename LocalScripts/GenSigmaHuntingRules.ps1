729c99538b1280ca11b30e58a4b02c5a49060f24
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-11 14:13:35 +0100

    master

---
+++ LocalScripts/GenSigmaHuntingRules.ps1
@@ -0,0 +1,47 @@
+param(
+    [Parameter(Mandatory=$true)]$HuntingRulesFolder
+)
+Install-Module powershell-yaml -AllowClobber -Scope CurrentUser -Force
+Import-Module powershell-yaml
+git clone https://github.com/Neo23x0/sigma.git tmp/Sigma
+$Date = Get-Date -Format "ddMMyyyy"
+$Files = Get-ChildItem -Path '.\tmp\Sigma\'  -Filter *.yaml -Recurse -File -Name | Select-String -Pattern "rules"
+$HuntingRulesTemplate = "{`r`n"
+$HuntingRulesTemplate += "  `"hunting`": [`r`n"
+foreach ($file in $Files){
+    if ($file -ne $Files[-1]) {
+        $content = ''
+        $filecontent = Get-Content -Path tmp\Azure-Sentinel\$file
+        foreach ($line in $filecontent) { $content = $content + "`n" + $line }
+        $huntingrules = ConvertFrom-YAML $content
+        $tactics = $huntingrules.tactics -join "`",`""
+        $HuntingRulesTemplate += "    {`r`n"
+        $HuntingRulesTemplate += "      `"author`": `"sigma`",`r`n"
+        $HuntingRulesTemplate += "      `"displayName`": `"" + $huntingrules.name + "`",`r`n"
+        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/Neo23x0/sigma/master/rules/network/" + $file.tostring().replace("\", "/").replace(" ","%20") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"description`": `"" + $huntingrules.description.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"query`": `"" + $huntingrules.query.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + $tactics + "`"`r`n      ]`r`n"
+        $HuntingRulesTemplate += "    },`r`n"
+    }
+    else {
+        $content = ''
+        $filecontent = Get-Content -Path tmp\Azure-Sentinel\$file
+        foreach ($line in $filecontent) { $content = $content + "`n" + $line }
+        $huntingrules = ConvertFrom-YAML $content
+        $tactics = $huntingrules.tactics -join "`",`""
+        $HuntingRulesTemplate += "    {`r`n"
+        $HuntingRulesTemplate += "      `"author`": `"sigma`",`r`n"
+        $HuntingRulesTemplate += "      `"displayName`": `"" + $huntingrules.name + "`",`r`n"
+        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/Neo23x0/sigma/master/rules/network/" + $file.tostring().replace("\", "/").replace(" ","%20") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"description`": `"" + $huntingrules.description.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"query`": `"" + $huntingrules.query.replace("\","\\").replace("`"","\`"").replace("`'","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + $tactics + "`"`r`n      ]`r`n"
+        $HuntingRulesTemplate += "    }`r`n"
+    }
+}
+$HuntingRulesTemplate += "  ]`r`n"
+$HuntingRulesTemplate += "}`r`n"
+Out-File -Path $HuntingRulesFolder\"MS_hunting-rules_"$Date".json" -InputObject $HuntingRulesTemplate
+Start-Sleep -s 20
+Remove-Item -Path tmp/Azure-Sentinel –Recurse -Force
