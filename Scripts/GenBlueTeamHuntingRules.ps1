6de6183779e045977aff36f4cf077eb8cd085903
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-11 16:01:19 +0100

    master

---
+++ Scripts/GenBlueTeamHuntingRules.ps1
@@ -0,0 +1,39 @@
+param(
+    [Parameter(Mandatory=$true)]$HuntingRulesFolder
+)
+git clone https://github.com/BlueTeamLabs/sentinel-attack.git tmp/BlueTeam_KQL
+$Date = Get-Date -Format "ddMMyyyy"
+$Files = Get-ChildItem -Path '.\tmp\BlueTeam_KQL\detections\'  -Filter *.txt -Recurse -File -Name
+$HuntingRulesTemplate = "{`r`n"
+$HuntingRulesTemplate += "  `"hunting`": [`r`n"
+foreach ($file in $Files){
+    if ($file -ne $Files[-1]) {
+        $filecontent = Get-Content -Path tmp\BlueTeam_KQL\$file
+        if ($null -ne $filecontent) {
+        $HuntingRulesTemplate += "    {`r`n"
+        $HuntingRulesTemplate += "      `"author`": `"blueteam`",`r`n"
+        $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/BlueTeamLabs/sentinel-attack/master/detections/" + $file.tostring() + "`",`r`n"
+        $HuntingRulesTemplate += "      `"description`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","").replace("`t","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"Collection`"`r`n      ]`r`n"
+        $HuntingRulesTemplate += "    },`r`n"}
+    }
+    else {
+        $filecontent = Get-Content -Path tmp\BlueTeam_KQL\$file
+        if ($null -ne $filecontent) {
+        $HuntingRulesTemplate += "    {`r`n"
+        $HuntingRulesTemplate += "      `"author`": `"blueteam`",`r`n"
+        $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/BlueTeamLabs/sentinel-attack/master/detections/" + $file.tostring() + "`",`r`n"
+        $HuntingRulesTemplate += "      `"description`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","").replace("`t","") + "`",`r`n"
+        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"Collection`"`r`n      ]`r`n"
+        $HuntingRulesTemplate += "    }`r`n"}
+    }
+}
+$HuntingRulesTemplate += "  ]`r`n"
+$HuntingRulesTemplate += "}`r`n"
+Out-File -Path $HuntingRulesFolder\"BlueTeam_hunting-rules_"$Date".json" -InputObject $HuntingRulesTemplate -Encoding ASCII
+Start-Sleep -s 20
+Remove-Item -Path tmp/BlueTeam_KQL –Recurse -Force
---
+++ Scripts/genmd_analyticsrules.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+with open(sys.argv[1], 'r') as huntingrules:
+    myjson = json.load(huntingrules)
+
+print('![](https://www.metsys.fr/wp-content/themes/metsys/images/svg/metsys-logo.svg "Metsys")')
+print('# Hunting Rules')
+
+for huntingrule in myjson:
+    for item in myjson[huntingrule]:
+        print('## ' + item["displayName"])
+        print('### Hunt Tags\n')
+        print('> Author: [' + item["author"] + '](https://www.metsys.fr/)' + '\n')
+        print('> Reference: [Link to medium post](' + item["reference"] + ')' + '\n')
+        print('### ATT&CK Tags\n')
+        print('> Tactics: ' + str(item["tactics"]) + '\n')
+        print('### Hunt details\n')
+        print('> Description: ' + item["description"] + '\n')
+        print('> Query:\n')
+        print('```C#' + item["query"] + '```')
