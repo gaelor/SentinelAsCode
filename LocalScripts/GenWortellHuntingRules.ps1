8b8c3af29e3a0cb655aa142ab37ffc9399cf710f
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-11 15:26:30 +0100

    master

--- LocalScripts/GenWortellHuntingRules.ps1
+++ LocalScripts/GenWortellHuntingRules.ps1
@@ -3,11 +3,13 @@ param(
 )
 git clone https://github.com/wortell/KQL.git tmp/Wortell_KQL
 $Date = Get-Date -Format "ddMMyyyy"
+$Excluded_Files = "KQL_win_sdbinst_shim_persistence.txt", "KQL_win_susp_calc", "KQL_win_susp_cli_escape"
+$regex = $Excluded_Files -join '|'
 $Files = Get-ChildItem -Path '.\tmp\Wortell_KQL\'  -Filter *.txt -Recurse -File -Name
 $HuntingRulesTemplate = "{`r`n"
 $HuntingRulesTemplate += "  `"hunting`": [`r`n"
 foreach ($file in $Files){
-    if ($file -ne $Files[-1] -And $file -ne "KQL_win_sdbinst_shim_persistence.txt", "KQL_win_susp_calc", "KQL_win_susp_cli_escape") {
+    if ($file -ne $Files[-1] -And $file -notmatch $regex ) {
         $filecontent = Get-Content -Path tmp\Wortell_KQL\$file
         if ($null -ne $filecontent) {
         $HuntingRulesTemplate += "    {`r`n"
@@ -19,7 +21,7 @@ foreach ($file in $Files){
         $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"Collection`"`r`n      ]`r`n"
         $HuntingRulesTemplate += "    },`r`n"}
     }
-    elseif ($file -eq $Files[-1] -And $file -ne "KQL_win_sdbinst_shim_persistence.txt", "KQL_win_susp_calc", "KQL_win_susp_cli_escape") {
+    elseif ($file -eq $Files[-1] -And $file -notmatch $regex) {
         $filecontent = Get-Content -Path tmp\Wortell_KQL\$file
         if ($null -ne $filecontent) {
         $HuntingRulesTemplate += "    {`r`n"
