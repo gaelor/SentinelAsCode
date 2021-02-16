0e113bba1f52aa0b9f30c5c1cb1ab39d54b9b8b4
gaelor <thomas.couilleaux@theclemvp.com>
2021-02-16 10:17:21 +0100

    master

--- Scripts/genmd_workbooks.py
+++ Scripts/genmd_workbooks.py
@@ -14,12 +14,12 @@ for key,workbook in myjson.items():
     if key == "parameters":
         print('## ' + filename)
         print('### Workbook Tags\n')
-        print('> Author: ['+ workbook["author"]["defaultValue"] + '](https://www.metsys.fr/)' + '\n')
+        print('> Author: ['+ workbook["author"]["defaultValue"].encode('utf-8') + '](https://www.metsys.fr/)' + '\n')
         print('> Reference: [Link to medium post](https://github.com/Azure/Azure-Sentinel/tree/master/' + sys.argv[1] + ')\n')
         print('### Workbook details\n')
-        print('> Description: ' + workbook["description"]["defaultValue"] + '\n')
+        print('> Description: ' + workbook["description"]["defaultValue"].encode('utf-8') + '\n')
     if key == "resources":
         print('> Query:\n')
         print('```t')
-        print(workbook[0]["properties"]["serializedData"])
+        print(workbook[0]["properties"]["serializedData"].encode('utf-8'))
         print('```\n')
