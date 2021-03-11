2da0fd789af1617a1f9d6062ff538ebfc5d6af14
gaelor <thomas.couilleaux@theclemvp.com>
2021-03-11 09:31:12 +0100

    dev

--- LocalScripts/GenBlueTeamHuntingRules.ps1
+++ LocalScripts/GenBlueTeamHuntingRules.ps1
@@ -10,26 +10,28 @@ foreach ($file in $Files){
     if ($file -ne $Files[-1]) {
         $filecontent = Get-Content -Path tmp\BlueTeam_KQL\detections\$file
         if ($null -ne $filecontent) {
-        $HuntingRulesTemplate += "    {`r`n"
-        $HuntingRulesTemplate += "      `"author`": `"blueteam`",`r`n"
-        $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/BlueTeamLabs/sentinel-attack/master/detections/" + $file.tostring() + "`",`r`n"
-        $HuntingRulesTemplate += "      `"description`": `"" + [regex]::Match($filecontent, "// Description: (.*?) //").Groups[1].Value.replace("#","").replace(", ","`",`"") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","").replace("`t","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + [regex]::Match($filecontent, "// Tactics: (.*?) //").Groups[1].Value.replace("#","").replace(", ","`",`"") + "`"`r`n      ]`r`n"
-        $HuntingRulesTemplate += "    },`r`n"}
+            $HuntingRulesTemplate += "    {`r`n"
+            $HuntingRulesTemplate += "      `"author`": `"blueteam`",`r`n"
+            $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+            $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/BlueTeamLabs/sentinel-attack/master/detections/" + $file.tostring() + "`",`r`n"
+            $HuntingRulesTemplate += "      `"description`": `"" + [regex]::Match($filecontent, "// Description: (.*?) //").Groups[1].Value.replace("#","").replace(", ","`",`"") + "`",`r`n"
+            $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","").replace("`t","") + "`",`r`n"
+            $HuntingRulesTemplate += "      `"tactics`": [`r`n`        " + [regex]::Match($filecontent, "// Tactics: (.*?) //").Groups[1].Value.replace(" #","`",`"").replace("#","`"").replace(",`",`"","`",`"").replace(" ", "") + "`"`r`n      ]`r`n"
+            $HuntingRulesTemplate += "    },`r`n"
+        }
     }
     else {
         $filecontent = Get-Content -Path tmp\BlueTeam_KQL\detections\$file
         if ($null -ne $filecontent) {
-        $HuntingRulesTemplate += "    {`r`n"
-        $HuntingRulesTemplate += "      `"author`": `"blueteam`",`r`n"
-        $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/BlueTeamLabs/sentinel-attack/master/detections/" + $file.tostring() + "`",`r`n"
-        $HuntingRulesTemplate += "      `"description`": `"" + [regex]::Match($filecontent, "// Description: (.*?) //").Groups[1].Value.replace("#","").replace(", ","`",`"") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","").replace("`t","") + "`",`r`n"
-        $HuntingRulesTemplate += "      `"tactics`": [`r`n`        `"" + [regex]::Match($filecontent, "// Tactics: (.*?) //").Groups[1].Value.replace("#","").replace(", ","`",`"") + "`"`r`n      ]`r`n"
-        $HuntingRulesTemplate += "    }`r`n"}
+            $HuntingRulesTemplate += "    {`r`n"
+            $HuntingRulesTemplate += "      `"author`": `"blueteam`",`r`n"
+            $HuntingRulesTemplate += "      `"displayName`": `"" + $file.tostring().replace(".txt","") + "`",`r`n"
+            $HuntingRulesTemplate += "      `"reference`": `"https://raw.githubusercontent.com/BlueTeamLabs/sentinel-attack/master/detections/" + $file.tostring() + "`",`r`n"
+            $HuntingRulesTemplate += "      `"description`": `"" + [regex]::Match($filecontent, "// Description: (.*?) //").Groups[1].Value.replace("#","").replace(", ","`",`"") + "`",`r`n"
+            $HuntingRulesTemplate += "      `"query`": `"" + $filecontent.replace("\","\\").replace("`"","\`"").replace("`'","").replace("`t","") + "`",`r`n"
+            $HuntingRulesTemplate += "      `"tactics`": [`r`n`        " + [regex]::Match($filecontent, "// Tactics: (.*?) //").Groups[1].Value.replace(" #","`",`"").replace("#","`"").replace(",`",`"","`",`"").replace(" ", "") + "`"`r`n      ]`r`n"
+            $HuntingRulesTemplate += "    }`r`n"
+        }
     }
 }
 $HuntingRulesTemplate += "  ]`r`n"
